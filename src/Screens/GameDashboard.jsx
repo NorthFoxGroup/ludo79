5650103d7370b91f898535db943dc26eeb0277bf
Prince K. Patel <56663010+princu09@users.noreply.github.com>
2022-09-11 14:43:18 +0530

    Folder Struct Completed & Routes Added

---
+++ src/Screens/GameDashboard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const GameDashboard = () => {
+  return (
+    <>
+    </>
+  )
+}
+
+export default GameDashboard
---
+++ src/Screens/index.js
@@ -0,0 +1,7 @@
+// Login & Signup
+import Login from "./Login";
+import Signup from "./Signup";
+
+import GameDashboard from "./GameDashboard";
+
+export { Login, Signup, GameDashboard };
